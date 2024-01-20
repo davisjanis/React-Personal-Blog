a6035da4cb7f8996583c2c8b1af07e7cb6e10a09
davisjanis <davislarins@gmail.com>
2024-01-20 20:33:14 +0100

    Created 'Sign Up' API route.

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -16,8 +17,12 @@ mongoose
 
 const app = express();
 
+// server accepts json as input
+app.use(express.json());
+
 app.listen(3002,()=> {
     console.log('Server is running on port 3002!')
 });
 
 app.use("/api/user", userRoutes);
+app.use('/api/auth', authRouter);
