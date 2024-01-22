33732cf0ba061734794293fc440f44735c2c910d
davisjanis <davislarins@gmail.com>
2024-01-22 19:38:39 +0100

    Updated user API route.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -20,9 +21,13 @@ const app = express();
 // server accepts json as input
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3002,()=> {
     console.log('Server is running on port 3002!')
 });
+
+
 
 app.use("/api/user", userRoutes);
 app.use('/api/auth', authRouter);
