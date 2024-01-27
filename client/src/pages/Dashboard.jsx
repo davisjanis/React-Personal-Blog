3f84c2c88069f8e4af2f94e043ea91d75518a611
davisjanis <davislarins@gmail.com>
2024-01-27 14:58:15 +0100

    Added 'show user posts' in dashbord functionality and 'delete post' functionality. Added 'update post' functionality.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+
 
 export default function Dashboard() {
   const location = useLocation();
@@ -28,7 +30,7 @@ export default function Dashboard() {
       {/* comments  */}
       {tab === 'comments' && <DashComments />}
       {/* dashboard comp */}
-      {tab === 'dash' && <DashboardComp />}
+      {/* {tab === 'dash' && <DashboardComp />} */}
     </div>
   );
 }
